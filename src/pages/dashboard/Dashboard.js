0863588bd244a4e64f99a780205dd31410345407
Marzan1810662 <hassanmarzan17@gmail.com>
2022-12-09 02:34:27 +0600

    dashboard drawer added

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
-        <div>
-            <p>Dashboard</p>
-            <aside>
-                <p>Side bar</p>
-            </aside>
-            <Outlet/>
-        </div>
+        <>
+            <div className="drawer drawer-mobile">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col items-center justify-center">
+                    {/* <!-- Page content here --> */}
+                    <Outlet />
+                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                    <ul className="menu w-80 py-4 text-primary-content bg-secondary">
+                        {/* <!-- Sidebar content here --> */}
+                        <li className='border-2 border-secondary-focus'><Link to='/dashboard/create-blog'>Create New Blog</Link></li>
+                        <li className='border-2 border-secondary-focus'><Link to='/dashboard/all-blogs'>ALL Blogs</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 };
 
