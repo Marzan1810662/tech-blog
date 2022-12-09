781bcc467f00bf16056ff1efe6af1694ac199e05
Marzan1810662 <hassanmarzan17@gmail.com>
2022-12-09 11:34:46 +0600

    all bloggs page

--- src/pages/dashboard/allBlogs/AllBlog.js
+++ src/pages/dashboard/allBlogs/AllBlog.js
@@ -2,8 +2,41 @@ import React from 'react';
 
 const AllBlog = () => {
     return (
-        <div className='text-center'>
-            <p>ALL blog</p>
+        <div className="overflow-x-auto w-5/6">
+            <table className="table w-full text-primary-focus border">
+                {/* <!-- head --> */}
+                <thead className='text-primary-content'>
+                    <tr>
+                        <th></th>
+                        <th>title</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* <!-- row 1 --> */}
+                    <tr className='border-2'>
+                        <th>1</th>
+                        <td>Cy Ganderton</td>
+                        <td>Quality Control Specialist</td>
+                        <td>Blue</td>
+                    </tr>
+                    {/* <!-- row 2 --> */}
+                    <tr className='border'>
+                        <th>2</th>
+                        <td>Hart Hagerty</td>
+                        <td>Desktop Support Technician</td>
+                        <td>Purple</td>
+                    </tr>
+                    {/* <!-- row 3 --> */}
+                    <tr className='border'>
+                        <th>3</th>
+                        <td>Brice Swyre</td>
+                        <td>Tax Accountant</td>
+                        <td>Red</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
